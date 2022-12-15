6e8538c05a5c91229be9e192716f0fe420e610f4
Byeolikim <213069@naver.com>
2022-12-15 21:54:46 +0900

    전역에 NavBar 컴포넌트 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NavBar from '../components/NavBar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>
+            {`
+              a { letter-spacing: -1px; }
+            `}
+        </style>
+    </div>
+  )
 }
