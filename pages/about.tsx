33fb8ae32dff828cf8e0a1c553f588b18d4c9b5d
Byeolikim <213069@naver.com>
2022-12-15 19:42:47 +0900

    about page

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar"
+export default function About() {
+    return (
+        <>
+            <NavBar />
+            <p>this is about page!</p>
+        </>
+    )
+}
