72c5ffd769538608f5a78ac3d0f7f5279523c533
Byeolikim <213069@naver.com>
2022-12-15 19:42:40 +0900

    main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import NavBar from '../components/NavBar';
 
-export default function Home() {
-  return (
-    <div>
-      <h1>/pages/index.js</h1>
-      <ul>
-        <li><a href="/sub">/pages/sub/index.tsx</a></li>
-        <li><a href="/sub/about">/pages/sub/index.tsx</a></li>
-        <li><a href="/sub/1">/pages/sub/[1].tsx</a></li>
-        <li><a href="/sub/2 ">/pages/sub/[1].tsx</a></li>
-        <li><a href="/sub/fetch">/pages/sub/fetch.tsx</a></li>
-      </ul>
-    </div>
-  )
+export default function Main () {
+    const [counter, setCounter] = useState(0);
+    return (
+        <>
+        <NavBar />
+        <div>
+            헬로 넥스트~ {counter}
+        </div>
+        <button onClick={() => setCounter((prev) => prev+1)}>+</button>
+        </>
+
+    )
 }
