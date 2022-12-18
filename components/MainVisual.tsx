4a9fe817443b0b84ef329ce464674ee0354cd3e9
Byeolikim <213069@naver.com>
2022-12-18 18:32:57 +0900

    main visual 수정

--- components/MainVisual.tsx
+++ components/MainVisual.tsx
@@ -1,11 +1,24 @@
+import Image from 'next/image'
+
 export default function MainVisual () {
     return(
         <>
             <section>
-                <img src='/profile_star.png' />
+                <div className='imgBox'>
+                    <Image
+                        src="/profile_star.png" alt="프로필 사진"
+                        width={390}
+                        height={500}
+                    />
+                </div>
             </section>
             <style jsx>
                 {`
+                    section { 
+                    display:flex;
+                    justify-content:center;
+                    align-items:center;
+                    height:100vh; }
                     section img { width:300px; }
                 `}
             </style>
