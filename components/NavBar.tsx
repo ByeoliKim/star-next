db7bcc01c150221e03078ec588c712963778ce11
Byeolikim <213069@naver.com>
2022-12-18 18:34:20 +0900

    NavBar 추가

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,30 +4,32 @@ import styles from "./NavBar.module.css";
 
 export default function NavBar () {
     const router = useRouter();
-    console.log(router);
+    //console.log(router);
     return (
         <>  
             <div className="top">
-                <h1>프론트엔드 개발자 김별이 ⭐️</h1>
+                <h1>개발자 김별이 ⭐️</h1>
                 <nav>
                     <Link href="/">
                         <a className={router.pathname === "/" ? "active" : ""}>Home</a>
                     </Link>
-                    <Link href="/about ">
+                    <Link href="/about">
                         <a className={router.pathname === "/about" ? "active" : ""}>About</a>
                     </Link>
-                    <Link href="/portfoilo ">
-                        <a className={router.pathname === "/portfoilo" ? "active" : ""}>portfoilo</a>
+                    <Link href="/portfoilo">
+                        <a className={router.pathname === "/portfoilo" ? "active" : ""}>Portfoilo</a>
+                    </Link>
+                    <Link href="/movie">
+                        <a className={router.pathname === "/movie" ? "active" : ""}>Movie</a>
                     </Link>
                 </nav>
             </div>
             <style jsx>
                 {`
-                    div.top { display:flex; align-items:center; justify-content:space-between; padding:0 20px; background-color:#1458a0; }
-                    h1 { color:#fff; }
+                    div.top { position:sticky; top:0; left:0; display:flex; align-items:center; justify-content:space-between; width:100%; padding:10px 20px; background-color:rgba(255,255,255,0.3); box-sizing:border-box; z-index:10; }
+                    h1 { color:#000; font-size:14px; font-weight:600; letter-spacing:-1px; cursor:default; }
                     nav { }
-                    nav > a { margin-left:20px; }
-                    a { color:#fff; font-size:18px; text-decoration:none; }
+                    a { margin-left:20px; color:#fff; font-size:14px; font-weight:400; text-decoration:none; }
                     .active { color:#000; }
                 `}
             </style>
